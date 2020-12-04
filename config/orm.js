486fecedba2aeac8803089bbf3ce5f2521275695
Crawford Smith <crawfordgsmith@gmail.com>
2020-12-03 21:35:50 -0500

    config files updated

---
+++ config/orm.js
@@ -0,0 +1,41 @@
+const connection = require("../config/connection.js");
+
+// Creates object 
+const orm = {
+    // Action SelectAll
+    selectAll: function (tableName, cb) {
+        const queryString = " SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    // Action InsertOne 
+    insertOne: function (tableName, column, value, cb) {
+        const queryString = `INSERT INTO ${tableName} (${column}, devoured) VALUES ("${value}", 0);`
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result)
+        });
+    },
+    // Action UpdateOne
+    updateOne: function (tableName, column, condition, value, cb) {
+        const queryString = `UPDATE ${tableName} SET ${column}=${condition} WHERE id=${value};`;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+
+    // Action Delete
+    delete: function (tableName, id, cb) {
+        const queryString = `DELETE FROM  ${tableName} WHERE id=${id};`
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+
+};
+
+module.exports = orm;
