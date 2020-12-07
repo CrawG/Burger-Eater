3c28239594ee8bc811b07920d388cc42deb51d8b
Crawford Smith <crawfordgsmith@gmail.com>
2020-12-07 15:18:18 -0500

    db schema and seeds files created

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+("Hamburger", false),
+("Cheeseburger", false), 
+("Bacon Cheeseburger", false),
+("All American Burger", false), 
+("Veggie Burger", false);
