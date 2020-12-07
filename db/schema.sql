7350a99b5e8d5f8c01065b3b6094af2e2aec1e87
Crawford Smith <crawfordgsmith@gmail.com>
2020-12-07 17:57:18 -0500

    js updates and package json completed

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
 CREATE TABLE burgers (
   id int AUTO_INCREMENT NOT NULL,
   burger_name varchar(255) NOT NULL,
---
+++ public/js/burgers.js
@@ -0,0 +1,60 @@
+// Click event to devour: 
+$(function () {
+    $(".change-devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var devoured = $(this).data("devoured");
+
+        var newDevouredState = {
+            devoured: devoured
+        };
+
+        // PUT request:
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("changed devoured to", devoured);
+
+                location.reload();
+            }
+        );
+    });
+    // Submit Click event:
+    $(".create-form").on("submit", function (event) {
+        // PreventDefault on a submit event: 
+        event.preventDefault();
+        var newBurger = {
+            burger_name: $("#ca").val().trim(),
+            devoured: false
+        };
+        console.log(newBurger.burger_name)
+        // POST request:
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+
+                location.reload();
+            }
+        );
+    });
+
+    // Delete event
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burger/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+
+                location.reload();
+            }
+        );
+    });
+
+});
